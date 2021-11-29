fc1bca5eb77d931089facde406c06088ee18533e
DESKTOP-EDOFNRP\klebe <kleber-rr@hotmail.com>
2021-11-29 08:12:08 -0400

    Aula 95

--- PhotosResourceServer/src/main/java/com/appsdeveloperblog/ws/api/photos/PhotosApplication.java
+++ PhotosResourceServer/src/main/java/com/appsdeveloperblog/ws/api/photos/PhotosApplication.java
@@ -2,7 +2,10 @@ package com.appsdeveloperblog.ws.api.photos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PhotosApplication {
 
--- ResourceServer/src/main/java/com/meudominio/ws/api/ResourceServer/controller/UsersController.java
+++ ResourceServer/src/main/java/com/meudominio/ws/api/ResourceServer/controller/UsersController.java
@@ -1,6 +1,8 @@
 package com.meudominio.ws.api.ResourceServer.controller;
 
 import com.meudominio.ws.api.ResourceServer.response.UserRest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,18 +14,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UsersController {
 
+    @Autowired
+    Environment env;
+
     @GetMapping("/status/check")
     public String status(){
-        return "Working...";
+        return "Working on port: " + env.getProperty("local.server.port");
     }
 
-//    @Secured("ROLE_developer")
     @PreAuthorize("hasAuthority('ROLE_developer') or #id == #jwt.subject")
     @DeleteMapping(path = "/{id}")
     public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
         return "Deleted user with id " + id + " and JWT subject " + jwt.getSubject();
     }
-
 
     @PostAuthorize("returnObject.userId == #id")
     @GetMapping(path = "/{id}")
