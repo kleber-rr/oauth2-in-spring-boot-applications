5096168525b40a637bc3f103f6ac4a7e20b31c7a
Kleber Santos <kleber.santos@sefaz.rr.gov.br>
2021-11-30 10:27:32 -0400

    curso finalizado

---
+++ MyRemoteUserStorageProvider/src/main/java/com/meudominio/keycloak/MyremoteUserStorageProvider/MyremoteUserStorageProviderApplication.java
@@ -0,0 +1,13 @@
+package com.meudominio.keycloak.MyremoteUserStorageProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyremoteUserStorageProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyremoteUserStorageProviderApplication.class, args);
+	}
+
+}
---
+++ MyRemoteUserStorageProvider/src/test/java/com/meudominio/keycloak/MyremoteUserStorageProvider/MyremoteUserStorageProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meudominio.keycloak.MyremoteUserStorageProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyremoteUserStorageProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
